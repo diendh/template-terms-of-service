8f3bc14b0e8e33b707cb00e71902d4c6f6057de8
Tom <diendh001@gmail.com>
2021-02-17 13:39:13 +0700

    Update get_match_day.py

--- get_match_day.py
+++ get_match_day.py
@@ -4,13 +4,21 @@
 
 import requests
 import pathlib
+import time
 
-CURRENT_PATH = pathlib.Path(__file__).parent.absolute()
-URL = "https://lh3.googleusercontent.com/proxy/lNLJCzO17HO_zyBXU1n2-S5EV1FCSh-sOOiL_GBFPzXGEWYXHRfX8srHvX3gPENbLEfbLPEJgyDebliTDZfQh5PjiQnSAu6YECdsjIUIHUeeP_y5P5Bx02LKUet8tKZfAfEJSvf-8pxVLmLpMVOHApsD3YTemk_Wr4bsCH0FlLfOJIvwphPL=s0"
+for x in range(10000):
 
-r = requests.get(url = URL, stream=True)
+    CURRENT_PATH = pathlib.Path(__file__).parent.absolute()
+    URL = "https://lh3.googleusercontent.com/proxy/lNLJCzO17HO_zyBXU1n2-S5EV1FCSh-sOOiL_GBFPzXGEWYXHRfX8srHvX3gPENbLEfbLPEJgyDebliTDZfQh5PjiQnSAu6YECdsjIUIHUeeP_y5P5Bx02LKUet8tKZfAfEJSvf-8pxVLmLpMVOHApsD3YTemk_Wr4bsCH0FlLfOJIvwphPL=s0"
 
-if r.status_code == 200:
-    with open('{}/img.png'.format(CURRENT_PATH), 'wb') as f:
-        for chunk in r:
-            f.write(chunk)
+    r = requests.get(url = URL, stream=True)
+
+    if r.status_code == 200:
+        with open('{}/img.png'.format(CURRENT_PATH), 'wb') as f:
+            for chunk in r:
+                f.write(chunk)
+    else:
+        with open('{}/err.log'.format(CURRENT_PATH), 'wb') as f:
+            for chunk in r:
+                f.write(chunk)
+    time.sleep(1)
