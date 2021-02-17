00e72a708895cecedace84c731306faf1642ec5b
Tom <diendh001@gmail.com>
2021-02-17 13:41:08 +0700

    Update get_match_day.py

--- get_match_day.py
+++ get_match_day.py
@@ -6,7 +6,7 @@ import requests
 import pathlib
 import time
 
-for x in range(10000):
+for x in range(1000):
 
     CURRENT_PATH = pathlib.Path(__file__).parent.absolute()
     URL = "https://lh3.googleusercontent.com/proxy/lNLJCzO17HO_zyBXU1n2-S5EV1FCSh-sOOiL_GBFPzXGEWYXHRfX8srHvX3gPENbLEfbLPEJgyDebliTDZfQh5PjiQnSAu6YECdsjIUIHUeeP_y5P5Bx02LKUet8tKZfAfEJSvf-8pxVLmLpMVOHApsD3YTemk_Wr4bsCH0FlLfOJIvwphPL=s0"
@@ -14,11 +14,14 @@ for x in range(10000):
     r = requests.get(url = URL, stream=True)
 
     if r.status_code == 200:
+        print('done')
         with open('{}/img.png'.format(CURRENT_PATH), 'wb') as f:
             for chunk in r:
                 f.write(chunk)
     else:
+        print(r.status_code)
         with open('{}/err.log'.format(CURRENT_PATH), 'wb') as f:
             for chunk in r:
                 f.write(chunk)
+    
     time.sleep(1)
